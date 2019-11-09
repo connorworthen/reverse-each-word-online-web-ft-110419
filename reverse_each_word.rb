76c29746e5f87864bee218bd3da3eb151dcacbbd
connor worthen <connorw220@protonmail.com>
2019-11-09 17:29:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,11 @@
-def reverse_each_word(string)
-  return string.split("").reverse.join("")
+def reverse(string)
+  reversed_string = ''
+
+ i = 0
+ while i < string.length
+  reversed_string = string[i] + reversed_string
+  i += 1
+ end
+
+ return reversed_string
 end
